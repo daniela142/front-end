af4c98ec11f4cb5e633b8e4527f6d677f9d4eaa1
trishmanjunath <trishati02@gmail.com>
2023-09-15 10:35:45 +1000

    sidebar, dashboard

---
+++ src/dashboard/components/sidebar.js
@@ -0,0 +1,6 @@
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+import Icon from "../svg/icon";
+import "../style/sidebar.css";
