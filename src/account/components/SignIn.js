5fc0f0f87dea3a172ab8fdcc1a1dd364937cd242
Quan Quan <79500388+QuanNguyenDong@users.noreply.github.com>
2023-09-15 13:53:38 +1000

    fix authentication screen

--- src/account/components/SignIn.js
+++ src/account/components/SignIn.js
@@ -1,75 +1,95 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
-import "../fonts/font.css"
+import "../fonts/font.css";
 import "../style/sign-in.css";
 import Icon from "../svg/icon";
 
-export const SignIn = _ => {
-    let navigate = useNavigate();
+export const SignIn = (_) => {
+  let navigate = useNavigate();
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        try {
-            const response = await axios.post(global.route + `/api/users/auth`, {
-                email: email,
-                password: password
-            }, { withCredentials: true });
-            localStorage.setItem("User", JSON.stringify(response.data));
-            navigate("/", { replace: true });
-        } catch (error) {
-            console.error(error);
-        }
-    };
+    try {
+      const response = await axios.post(
+        global.route + `/api/users/auth`,
+        {
+          email: email,
+          password: password,
+        },
+        { withCredentials: true }
+      );
+      localStorage.setItem("User", JSON.stringify(response.data));
+      navigate("/", { replace: true });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    const handleKeypress = e => {
-        if(e.key === "Enter"){
-            SignIn();
-        }
-    };
+  const handleKeypress = (e) => {
+    if (e.key === "Enter") {
+      SignIn();
+    }
+  };
 
-    return(
-        <div className="background">
-            <div className="primary-box">
-                <div className="title"><h1><Icon/> Académy</h1></div>
-                    <div className="user-authentication-input">
-                        <label html="email">Email Address/Username</label>
-                    <input 
-                        id="email"
-                        name="email"
-                        type="email"
-                        value={email} 
-                        onChange={(e) => setEmail(e.target.value)}
-                        placeholder="johndoe@gmail.com"/>
-                    </div>
-                    <div className="user-authentication-input">
-                        <label html="password">Password</label>
-                        <input 
-                            id="password"
-                            name="password"
-                            type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)} 
-                            onKeyPress={(e) => handleKeypress(e)}/>
-                    </div>
-                        <button 
-                            style={{cursor: "pointer"}}
-                            className="sign-in-button"
-                            onSubmit={(e) => handleSubmit(e)}
-                            onClick={(e) => handleSubmit(e)}>
-                            Sign In
-                        </button>
-                    
-                    <div className='sign-up-text'>
-                        <p>Don't have an account? <text style={{color: "#3592c1", textDecoration: "underline", cursor: "pointer"}} onClick ={(e) => navigate("/signUp", {replace: true})}>Sign Up</text> </p>
-                    </div>
-            </div>
+  return (
+    <div className="background">
+      <div className="primary-box">
+        <div style={{ height: "78px" }}></div>
+        <div className="title">
+          <h1>
+            <Icon /> Académy
+          </h1>
         </div>
-    );
-
-}
+        <div className="user-authentication-input">
+          <label html="email">Email Address/Username</label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="johndoe@gmail.com"
+          />
+        </div>
+        <div className="user-authentication-input">
+          <label html="password">Password</label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            onKeyPress={(e) => handleKeypress(e)}
+          />
+        </div>
+        <button
+          style={{ cursor: "pointer" }}
+          className="sign-in-button"
+          onSubmit={(e) => handleSubmit(e)}
+          onClick={(e) => handleSubmit(e)}
+        >
+          Sign In
+        </button>
+        <div className="sign-up-text">
+          <p>Don't have an account?</p>
+          <text
+            style={{
+              color: "#3592c1",
+              textDecoration: "underline",
+              cursor: "pointer",
+            }}
+            onClick={(e) => navigate("/signUp", { replace: true })}
+          >
+            Sign Up
+          </text>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/account/components/SignUp.js
+++ src/account/components/SignUp.js
@@ -1,88 +1,94 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
-import "../fonts/font.css"
+import "../fonts/font.css";
 import "../style/sign-up.css";
 import Icon from "../svg/icon";
 import Back from "../svg/back";
 
-export const SignUp = _ => {
-    let navigate = useNavigate();
+export const SignUp = (_) => {
+  let navigate = useNavigate();
 
-    const [username, setUsername] = useState("");
-    const [firstname, setFirstname] = useState("");
-    const [lastname, setLastname] = useState("");
+  const [username, setUsername] = useState("");
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        try {
-            const response = await axios.post(global.route + `/api/users`, {
-                username: username,
-                firstname: firstname,
-                lastname: lastname,
-                email: email,
-                password: password
-            }, { withCredentials: true });
-            localStorage.setItem("User", JSON.stringify(response.data));
-            navigate("/", { replace: true });
-        } catch (error) {
-            console.error(error);
-        }
-    };
+    try {
+      const response = await axios.post(
+        global.route + `/api/users`,
+        {
+          username: username,
+          firstname: firstname,
+          lastname: lastname,
+          email: email,
+          password: password,
+        },
+        { withCredentials: true }
+      );
+      localStorage.setItem("User", JSON.stringify(response.data));
+      navigate("/", { replace: true });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    return(
-        <div className="background">
-            <div className="primary-box-sign-up">
-                <div> <a href='/SignIn'><button className='back-button'><Back/></button></a></div>
-                <div className="title"><h1><Icon/> Académy</h1></div>
-                    <div className="user-authentication-input">
-                        <label html="email">Email Address</label>
-                        <input 
-                            id="email2"
-                            name="email"
-                            type="email"
-                            value={email} 
-                            onChange={(e) => setEmail(e.target.value)}
-                            placeholder="johndoe@gmail.com"/>
-                    </div>
-                    <div className='user-authentication-input'>
-                        <label>Username</label>
-                        <input
-                            id="username"
-                            name="username"
-                            type="text"
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                            />
-                    </div>
-                    <div className='auth-name user-authentication-input'>
-                        <div>
-                            <label>First Name</label>
-                            <input
-                                id="firstname"
-                                name="firstname"
-                                type="text"
-                                placeholder='John'
-                                onChange={(e) => setFirstname(e.target.value)}
-                                />
-                        </div>
-                        <div>
-                            <label>Last Name</label>
-                            <input
-                                id="lastname"
-                                name="lastname"
-                                type="text"
-                                placeholder='Doe'
-                                onChange={(e) => setLastname(e.target.value)}
-                                />
-                        </div>
-                    </div>
-                    {/* <table className='first-last-name' style={{width: "60px"}}>
+  return (
+    <div className="background">
+      <div className="primary-box-sign-up">
+        <div className="box-top">
+          <a href="/SignIn">
+            <button className="back-button">
+              <Back />
+            </button>
+          </a>
+          <p>Step 1 of 2</p>
+        </div>
+        <div className="title">
+          <h1>
+            <Icon /> Académy
+          </h1>
+        </div>
+        <div className="user-authentication-input">
+          <label html="email">Email Address</label>
+          <input
+            id="email2"
+            name="email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="johndoe@gmail.com"
+          />
+        </div>
+        <div className="auth-name">
+          <div>
+            <label>First Name</label>
+            <input
+              id="firstname"
+              name="firstname"
+              type="text"
+              placeholder="John"
+              onChange={(e) => setFirstname(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Last Name</label>
+            <input
+              id="lastname"
+              name="lastname"
+              type="text"
+              placeholder="Doe"
+              onChange={(e) => setLastname(e.target.value)}
+            />
+          </div>
+        </div>
+        {/* <table className='first-last-name' style={{width: "60px"}}>
                         <tr>
                             <td style={{fontSize: '16px'}}>First Name</td>
                             <td style={{fontSize: '16px'}}>Last Name</td>
@@ -104,25 +110,26 @@ export const SignUp = _ => {
                                 /></td>
                         </tr>
                     </table> */}
-                    <div className="user-authentication-input">
-                        <label html="password">Password</label>
-                        <input 
-                            id="password2"
-                            name="password"
-                            type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}/>
-                    </div>
-                    <div>
-                        <button 
-                            className="sign-up-button"
-                            onSubmit={(e) => handleSubmit(e)}
-                            onClick={(e) => handleSubmit(e)}>
-                            Sign Up
-                        </button>
-                    </div>
-            </div>
+        <div className="user-authentication-input">
+          <label html="password">Password</label>
+          <input
+            id="password2"
+            name="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
-    );
-
-}
+        <div>
+          <button
+            className="sign-up-button"
+            onSubmit={(e) => handleSubmit(e)}
+            onClick={(e) => handleSubmit(e)}
+          >
+            Sign Up
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
