31543a3f08046e796e879fdf681145381c429d5a
jarrodfeaks <85427306+jarrodfeaks@users.noreply.github.com>
2023-10-11 22:24:21 +1100

    signup UX improvements

---
+++ src/account/components/LoadingCircle.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../style/loading-circle.css';
+
+const LoadingCircle = () => {
+    return <div className="loading-circle"></div>;
+}
+
+export default LoadingCircle;
