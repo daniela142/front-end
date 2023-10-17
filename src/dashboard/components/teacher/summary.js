868229382391cb5dc229be36564d679654b1df0d
trishmanjunath <108712861+trishmanjunath@users.noreply.github.com>
2023-10-17 12:51:44 +1100

    teacher courses updated

--- src/dashboard/components/teacher/summary.js
+++ src/dashboard/components/teacher/summary.js
@@ -2,52 +2,34 @@ import React from "react";
 import "../../style/courses.css";
 import "../../fonts/font.css";
 import "../../style/summary.css"
+import { Line } from "rc-progress";
+import CourseIcon from "../../svg/courseIcon";
 
 export const Summary = () => {
 
     return (
         <div className="summary-box">
-            <label className="summary-title">Dashboard</label>
-
-            <div className="summary-container">
-                <div className="active-courses">
-                    <div>
-                        <label className="title"> Active Courses </label>
-                    </div>
-                    <div style={{ textAlign: "center", marginTop: "9px" }}>
-                        <label className="value"> 2 </label>
-                    </div>
-                </div>
-
-                <div className="total-courses">
-                    <div>
-                        <label className="title"> Total Courses </label>
-                    </div>
-                    <div style={{ textAlign: "center", marginTop: "9px" }}>
-                        <label className="value"> 5 </label>
-                    </div>
-                </div>
+            <div className="summary-title">Course Progress</div>
+        
+           <div> 
+            <div className="course-row">
+            <CourseIcon/>
+            <text style={{display:"inline-flex", marginLeft:"20px"}}>Web Design</text>
+            <Line percent={87} strokeWidth={3} strokeColor="#5855D6" style={{ marginTop:"30px", marginLeft:"-90px", height:"13px", display:"inline-flex"}}/> <text style={{position:"relative", marginTop:"25px", marginLeft:"10px"}}>87%</text>
             </div>
-
-            <div className="summary-container">
-                <div className="total-students">
-                    <div>
-                        <label className="title"> Total Students </label>
-                    </div>
-                    <div style={{ textAlign: "center", marginTop: "9px" }}>
-                        <label className="value"> 45 </label>
-                    </div>
-                </div>
-
-                <div className="completed-courses">
-                    <div>
-                        <label className="title"> Active Courses </label>
-                    </div>
-                    <div style={{ textAlign: "center", marginTop: "9px" }}>
-                        <label className="value"> 2 </label>
-                    </div>
-                </div>
+            <div className="course-row">
+            <CourseIcon/>
+            <text style={{display:"inline-flex", marginLeft:"20px"}}>Development Basics</text>
+            <Line percent={63} strokeWidth={3} strokeColor="#5855D6" style={{ marginTop:"30px", marginLeft:"-148px", height:"13px", display:"inline-flex"}}/> <text style={{position:"relative", marginTop:"25px", marginLeft:"10px"}}>63%</text>
             </div>
+            <div className="course-row">
+            <CourseIcon/>
+            <text style={{display:"inline-flex", marginLeft:"20px"}}>Data with Python</text>
+            <Line percent={71} strokeWidth={3} strokeColor="#5855D6" style={{ marginTop:"30px", marginLeft:"-125px", height:"13px", display:"inline-flex"}}/> <text style={{position:"relative", marginTop:"25px", marginLeft:"10px"}}>71%</text>
+            </div>
+            
+           </div>
+           
         </div>
     );
 }
