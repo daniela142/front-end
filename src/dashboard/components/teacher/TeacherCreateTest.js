488e5010850b9a28d2c1330eb5da6923a093ab60
trishmanjunath <108712861+trishmanjunath@users.noreply.github.com>
2023-10-20 12:10:27 +1100

    add question options to create test

--- src/dashboard/components/teacher/TeacherCreateTest.js
+++ src/dashboard/components/teacher/TeacherCreateTest.js
@@ -23,23 +23,31 @@ export const TeacherCreateTest = () => {
             </div>
             <hr className="line"></hr>
             <div>
-                <div className="option">
+                <div>
+                <input type="radio" style={{width:"18px", height:"18px", outline:"none", border:"0.5px solid #6d6b6b;", display:"block", alignItems:"center", marginTop:"20px"}}></input>
                 <input className="question-option-text"
+                        style={{marginTop:"-22px"}}
                         type="text"
                         placeholder="Option 1"></input>
                 </div>
-                <div className="option">
+                <div>
+                <input type="radio" style={{width:"18px", height:"18px", outline:"none", border:"0.5px solid #6d6b6b;", display:"block", alignItems:"center", marginTop:"20px"}}></input>
                 <input className="question-option-text"
+                        style={{marginTop:"-22px"}}
                         type="text"
                         placeholder="Option 2"></input>
                 </div>
-                <div className="option">
+                <div>
+                <input type="radio" style={{width:"18px", height:"18px", outline:"none", border:"0.5px solid #6d6b6b;", display:"block", alignItems:"center", marginTop:"20px"}}></input>
                 <input className="question-option-text"
+                        style={{marginTop:"-22px"}}
                         type="text"
                         placeholder="Option 3"></input>
                 </div>
-                <div className="option">
+                <div>
+                <input type="radio" style={{width:"18px", height:"18px", outline:"none", border:"0.5px solid #6d6b6b;", display:"block", alignItems:"center", marginTop:"20px"}}></input>
                 <input className="question-option-text"
+                        style={{marginTop:"-22px"}}
                         type="text"
                         placeholder="Option 4"></input>
                 </div>
@@ -58,6 +66,15 @@ export const TeacherCreateTest = () => {
                 </input>
             </div>              
         </div>
+        <div style={{display:"block"}}>
+        <div className="add-mcq-question-box">
+            <button className="add-question-button"> + Add multiple choice question</button>
+        </div>
+        <div className="add-mcq-question-box">
+            <button className="add-question-button"> + Add short answer question</button>
+        </div>
+        </div>
+        
     </div> 
     );
 }
