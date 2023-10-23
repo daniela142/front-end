8dfc21dd4828780d130da646d1233d257c7244e2
Daniel Abdelsayed <daniel.abdelsayed1@gmail.com>
2023-10-23 21:18:19 +1100

    added badge selection UI, fixed 'add question' button render

--- src/dashboard/components/teacher/TeacherCreateTest.js
+++ src/dashboard/components/teacher/TeacherCreateTest.js
@@ -7,9 +7,13 @@ import BadgeIcon from "../../svg/badgeIcon";
 
 export const TeacherCreateTest = () => {
 
-  const [badge, setBadge] = useState("");
+  const [activeBadge, setActiveBadge] = useState(null);
+  const badges = ['#518CFE', '#6D5ED2', '#5ED278', '#F4694C', '#F4D94C'];
+  const inactiveBadge = '#ABABAB'
 
-//   const handleBadgeChange = async() => {}
+  const handleBadgeChange = (buttonId) => {
+    setActiveBadge(buttonId);
+  };
 
   function disableRadio(radioButton) {
     // Disable the radio button
@@ -27,7 +31,7 @@ export const TeacherCreateTest = () => {
     // newComponent.id = "newComponentID";
 
     // Append the new component to the parent container
-    var container = document.getElementById("form-clones");
+    var container = document.getElementById("questions-container");
     container.appendChild(newComponent);
 
     //duplicateButtons();
@@ -44,7 +48,7 @@ export const TeacherCreateTest = () => {
     // newComponent.id = "newComponentID";
 
     // Append the new component to the parent container
-    var container = document.getElementById("form-clones");
+    var container = document.getElementById("questions-container");
     container.appendChild(newComponent);
 
     // duplicateButtons();
@@ -67,173 +71,169 @@ export const TeacherCreateTest = () => {
 
 
   return (
-    <div className="background-style">
-      <div className="create-test-title">
-        <input
-          className="input-text"
-          type="text"
-          placeholder="Title of Quiz"
-        ></input>
-      </div>
-      <div className="mcq-box" id="mcq-form">
-        <div>
+    <div>
+      <div className="background-style" id="questions-container">
+        <div className="create-test-title">
+          <input
+            className="input-text"
+            type="text"
+            placeholder="Title of Quiz"
+          ></input>
+        </div>
+        <div className="mcq-box" id="mcq-form">
+          <div style={{ display: "flex" }}>
+            <input
+              className="question-input-text"
+              type="text"
+              placeholder="Question..."
+            ></input>
+
+            <div>
+              {badges.map((badgeId) => (
+                <button
+                  className="badge-button"
+                  key={badgeId}
+                  // className={activeBadge === badgeId ? 'active' : ''}
+                  onClick={() => handleBadgeChange(badgeId)}
+                >
+                  <BadgeIcon fill={activeBadge === badgeId ? badgeId : inactiveBadge} />
+                </button>
+              ))}
+            </div>
+          </div>
+          <hr className="line"></hr>
+          <div>
+            <form>
+              <input
+                type="radio"
+                id="option1"
+                onClick={disableRadio}
+                style={{
+                  width: "18px",
+                  height: "18px",
+                  outline: "none",
+                  border: "0.5px solid #6d6b6b;",
+                  display: "block",
+                  alignItems: "center",
+                  marginTop: "20px",
+                }}
+              ></input>
+              <input
+                className="question-option-text"
+                style={{ marginTop: "-22px" }}
+                type="text"
+                placeholder="Option 1"
+              ></input>
+
+              <input
+                type="radio"
+                id="option2"
+                onClick={disableRadio}
+                style={{
+                  width: "18px",
+                  height: "18px",
+                  outline: "none",
+                  border: "0.5px solid #6d6b6b;",
+                  display: "block",
+                  alignItems: "center",
+                  marginTop: "20px",
+                }}
+              ></input>
+              <input
+                className="question-option-text"
+                style={{ marginTop: "-22px" }}
+                type="text"
+                placeholder="Option 2"
+              ></input>
+
+              <input
+                type="radio"
+                id="option3"
+                onClick={disableRadio}
+                style={{
+                  width: "18px",
+                  height: "18px",
+                  outline: "none",
+                  border: "0.5px solid #6d6b6b;",
+                  display: "block",
+                  alignItems: "center",
+                  marginTop: "20px",
+                }}
+              ></input>
+              <input
+                className="question-option-text"
+                style={{ marginTop: "-22px" }}
+                type="text"
+                placeholder="Option 3"
+              ></input>
+
+              <input
+                type="radio"
+                id="option4"
+                onClick={disableRadio}
+                style={{
+                  width: "18px",
+                  height: "18px",
+                  outline: "none",
+                  border: "0.5px solid #6d6b6b;",
+                  display: "block",
+                  alignItems: "center",
+                  marginTop: "20px",
+                }}
+              ></input>
+              <input
+                className="question-option-text"
+                style={{ marginTop: "-22px" }}
+                type="text"
+                placeholder="Option 4"
+              ></input>
+            </form>
+          </div>
+        </div>
+
+        <div className="short-answer-box" id="sa-form">
           <input
             className="question-input-text"
             type="text"
             placeholder="Question..."
           ></input>
-          <button className="badge-button" onClick={() => setBadge("wood")}>
-                <BadgeIcon fill="#518CFE" />
-           </button>
-          {/* <button className="badge-button" onClick={() => setBadge("iron")}>
-            {
-              (badge = "iron" ? (
-                <BadgeIcon fill="#6D5ED2" />
-              ) : (
-                <BadgeIcon fill="#5ED278" />
-              ))
-            } */}
-          {/* </button> */} 
-          {/* <button className="badge-button" onClick={()=>setBadge("silver")}><BadgeIcon/></button>
-            <button className="badge-button" onClick={()=>setBadge("gold")}><BadgeIcon/></button>
-            <button className="badge-button" onClick={()=>setBadge("diamond")}><BadgeIcon/></button> */}
-        </div>
-        <hr className="line"></hr>
-        <div>
-          <form>
+          <button className="badge-button">
+            <BadgeIcon />
+          </button>
+          <button className="badge-button">
+            <BadgeIcon />
+          </button>
+          <button className="badge-button">
+            <BadgeIcon />
+          </button>
+          <button className="badge-button">
+            <BadgeIcon />
+          </button>
+          <button className="badge-button">
+            <BadgeIcon />
+          </button>
+          <hr className="line"></hr>
+          <div>
             <input
-              type="radio"
-              id="option1"
-              onClick={disableRadio}
-              style={{
-                width: "18px",
-                height: "18px",
-                outline: "none",
-                border: "0.5px solid #6d6b6b;",
-                display: "block",
-                alignItems: "center",
-                marginTop: "20px",
-              }}
-            ></input>
-            <input
-              className="question-option-text"
-              style={{ marginTop: "-22px" }}
+              className="sa-option-text"
               type="text"
-              placeholder="Option 1"
+              placeholder="Type your answer..."
             ></input>
-
-            <input
-              type="radio"
-              id="option2"
-              onClick={disableRadio}
-              style={{
-                width: "18px",
-                height: "18px",
-                outline: "none",
-                border: "0.5px solid #6d6b6b;",
-                display: "block",
-                alignItems: "center",
-                marginTop: "20px",
-              }}
-            ></input>
-            <input
-              className="question-option-text"
-              style={{ marginTop: "-22px" }}
-              type="text"
-              placeholder="Option 2"
-            ></input>
-
-            <input
-              type="radio"
-              id="option3"
-              onClick={disableRadio}
-              style={{
-                width: "18px",
-                height: "18px",
-                outline: "none",
-                border: "0.5px solid #6d6b6b;",
-                display: "block",
-                alignItems: "center",
-                marginTop: "20px",
-              }}
-            ></input>
-            <input
-              className="question-option-text"
-              style={{ marginTop: "-22px" }}
-              type="text"
-              placeholder="Option 3"
-            ></input>
-
-            <input
-              type="radio"
-              id="option4"
-              onClick={disableRadio}
-              style={{
-                width: "18px",
-                height: "18px",
-                outline: "none",
-                border: "0.5px solid #6d6b6b;",
-                display: "block",
-                alignItems: "center",
-                marginTop: "20px",
-              }}
-            ></input>
-            <input
-              className="question-option-text"
-              style={{ marginTop: "-22px" }}
-              type="text"
-              placeholder="Option 4"
-            ></input>
-          </form>
-        </div>
-      </div>
-
-      <div className="short-answer-box" id="sa-form">
-        <input
-          className="question-input-text"
-          type="text"
-          placeholder="Question..."
-        ></input>
-        <button className="badge-button">
-          <BadgeIcon />
-        </button>
-        <button className="badge-button">
-          <BadgeIcon />
-        </button>
-        <button className="badge-button">
-          <BadgeIcon />
-        </button>
-        <button className="badge-button">
-          <BadgeIcon />
-        </button>
-        <button className="badge-button">
-          <BadgeIcon />
-        </button>
-        <hr className="line"></hr>
-        <div>
-          <input
-            className="sa-option-text"
-            type="text"
-            placeholder="Type your answer..."
-          ></input>
-        </div>
-      </div>
-      <div id="buttons">
-        <div id="form-clones" style={{ display: "block" }}>
-          <div className="add-mcq-question-box" onClick={duplicateMCQ}>
-            <button className="add-question-button">
-              {" "}
-              + Add multiple choice question
-            </button>
           </div>
-          <div className="add-mcq-question-box">
-            <button
-              className="add-question-button"
-              onClick={duplicateShortAnswer}
-            >
-              {" "}
+        </div>
+      </div>
+
+      <div id="buttons">
+        <div id="form-clones" style={{ display: "block", paddingBottom: "50px" }}>
+          <div className="add-mcq-question-box" onClick={duplicateMCQ}>
+            <text className="add-question-button">
+              + Add multiple choice question
+            </text>
+          </div>
+          <div className="add-mcq-question-box" onClick={duplicateShortAnswer}>
+            <text className="add-question-button">
               + Add short answer question
-            </button>
+            </text>
           </div>
         </div>
       </div>
--- src/dashboard/svg/badgeIcon.js
+++ src/dashboard/svg/badgeIcon.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 
-function Icon(fill) {
+function Icon({fill}) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
