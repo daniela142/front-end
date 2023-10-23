1eb2fbcecf8f916068fe92b38909b2635926b18b
jarrodfeaks <85427306+jarrodfeaks@users.noreply.github.com>
2023-10-23 17:43:09 +1100

    merge student exam pages

---
+++ src/dashboard/components/student/Exam.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import "../../style/exam.css";
+
+
+
+export const Exam = () => {
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleSubmit = () => {
+    if (selectedOption === '32') {
+      alert('Correct!');
+    } else {
+      alert('Incorrect');
+    }
+  };
+
+  return (
+    <div className="Exam">
+    <header>Introduction to Development Basics</header>
+    <div className="question-box">
+      <h2>Question 3</h2>
+      <div className="question">
+        How many teeth does the average human have?
+        <form onSubmit={e => e.preventDefault()} className="options">
+          {["62", "32", "9", "None of the above"].map(option => (
+            <label key={option}>
+              <input
+                type="radio"
+                value={option}
+                checked={selectedOption === option}
+                onChange={e => setSelectedOption(e.target.value)}
+              />
+              {option}
+            </label>
+          ))}
+        </form>
+      </div>
+    </div>
+    <button onClick={handleSubmit}>Submit</button>
+
+  </div>
+);
+}
+
+
+
