af836cb145fa24e501b5203c160ba003ba3d8c2c
trishmanjunath <108712861+trishmanjunath@users.noreply.github.com>
2023-10-23 16:26:00 +1100

    added 'add question' function and  badge icons

--- src/dashboard/components/teacher/TeacherCreateTest.js
+++ src/dashboard/components/teacher/TeacherCreateTest.js
@@ -1,63 +1,128 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../../style/dashboard.css";
 import "../../style/teachercreatetest.css";
 import "../../fonts/font.css"
+import BadgeIcon from "../../svg/badgeIcon";
 
 export const TeacherCreateTest = () => {
 
+    function disableRadio(radioButton) {
+        // Disable the radio button
+        radioButton.disabled = true;
+    }
+
+    function duplicateMCQ() {
+        // Find the element you want to duplicate by its ID
+        var originalComponent = document.getElementById("mcq-form");
+
+        // Create a new element (a clone) based on the original component
+        var newComponent = originalComponent.cloneNode(true);
+
+        // Update the ID or other attributes of the new component if necessary
+        // newComponent.id = "newComponentID";
+
+        // Append the new component to the parent container
+        var container = document.getElementById("form-clones");
+        container.appendChild(newComponent);
+
+        //duplicateButtons();
+    }
+
+
+    function duplicateShortAnswer() {
+        // Find the element you want to duplicate by its ID
+        var originalComponent = document.getElementById("sa-form");
+
+        // Create a new element (a clone) based on the original component
+        var newComponent = originalComponent.cloneNode(true);
+
+        // Update the ID or other attributes of the new component if necessary
+        // newComponent.id = "newComponentID";
+
+        // Append the new component to the parent container
+        var container = document.getElementById("form-clones");
+        container.appendChild(newComponent);
+
+        // duplicateButtons();
+    }
+
+    // function duplicateButtons() {
+    //     // Find the element you want to duplicate by its ID
+    //     var originalComponent = document.getElementById("q-add-button1");
+
+    //     // Create a new element (a clone) based on the original component
+    //     var newComponent = originalComponent.cloneNode(true);
+
+    //     // Update the ID or other attributes of the new component if necessary
+    //     // newComponent.id = "newComponentID";
+
+    //     // Append the new component to the parent container
+    //     var container = document.getElementById("buttons");
+    //     container.appendChild(newComponent);
+    // }
+
+
     return (
-       <div>
+       <div className="background-style">
         <div className="create-test-title">
             <input className="input-text" 
                     type="text"
                     placeholder="Title of Quiz">
-            </input>
+            </input> 
         </div>
-        <div className="mcq-box">
+        <div className="mcq-box" id="mcq-form">
             <div>
             <input className="question-input-text"
                     type="text"
                     placeholder="Question..."> 
             </input>
+            <button className="badge-button"><BadgeIcon/></button>
+            <button className="badge-button"><BadgeIcon/></button>
+            <button className="badge-button"><BadgeIcon/></button>
+            <button className="badge-button"><BadgeIcon/></button>
+            <button className="badge-button"><BadgeIcon/></button>
             </div>
             <hr className="line"></hr>
             <div>
-                <div>
-                <input type="radio" style={{width:"18px", height:"18px", outline:"none", border:"0.5px solid #6d6b6b;", display:"block", alignItems:"center", marginTop:"20px"}}></input>
+                <form>
+                <input type="radio" id= "option1" onClick={disableRadio} style={{width:"18px", height:"18px", outline:"none", border:"0.5px solid #6d6b6b;", display:"block", alignItems:"center", marginTop:"20px"}}></input>
                 <input className="question-option-text"
                         style={{marginTop:"-22px"}}
                         type="text"
                         placeholder="Option 1"></input>
-                </div>
-                <div>
-                <input type="radio" style={{width:"18px", height:"18px", outline:"none", border:"0.5px solid #6d6b6b;", display:"block", alignItems:"center", marginTop:"20px"}}></input>
+               
+                
+                <input type="radio" id= "option2" onClick={disableRadio} style={{width:"18px", height:"18px", outline:"none", border:"0.5px solid #6d6b6b;", display:"block", alignItems:"center", marginTop:"20px"}}></input>
                 <input className="question-option-text"
                         style={{marginTop:"-22px"}}
                         type="text"
                         placeholder="Option 2"></input>
-                </div>
-                <div>
-                <input type="radio" style={{width:"18px", height:"18px", outline:"none", border:"0.5px solid #6d6b6b;", display:"block", alignItems:"center", marginTop:"20px"}}></input>
+                
+                <input type="radio" id= "option3" onClick={disableRadio} style={{width:"18px", height:"18px", outline:"none", border:"0.5px solid #6d6b6b;", display:"block", alignItems:"center", marginTop:"20px"}}></input>
                 <input className="question-option-text"
                         style={{marginTop:"-22px"}}
                         type="text"
                         placeholder="Option 3"></input>
-                </div>
-                <div>
-                <input type="radio" style={{width:"18px", height:"18px", outline:"none", border:"0.5px solid #6d6b6b;", display:"block", alignItems:"center", marginTop:"20px"}}></input>
+               
+                <input type="radio" id= "option4" onClick={disableRadio} style={{width:"18px", height:"18px", outline:"none", border:"0.5px solid #6d6b6b;", display:"block", alignItems:"center", marginTop:"20px"}}></input>
                 <input className="question-option-text"
                         style={{marginTop:"-22px"}}
                         type="text"
                         placeholder="Option 4"></input>
-                </div>
+                </form>
             </div>
         </div>
 
-        <div className="short-answer-box">
+        <div className="short-answer-box" id="sa-form">
             <input className="question-input-text"
                     type="text"
                     placeholder="Question..."></input>
+            <button className="badge-button"><BadgeIcon/></button>
+            <button className="badge-button"><BadgeIcon/></button>
+            <button className="badge-button"><BadgeIcon/></button>
+            <button className="badge-button"><BadgeIcon/></button>
+            <button className="badge-button"><BadgeIcon/></button>
              <hr className="line"></hr>
             <div>
                 <input className="sa-option-text"
@@ -66,15 +131,16 @@ export const TeacherCreateTest = () => {
                 </input>
             </div>              
         </div>
-        <div style={{display:"block"}}>
-        <div className="add-mcq-question-box">
-            <button className="add-question-button"> + Add multiple choice question</button>
+        <div id="buttons">
+            <div id= "form-clones" style={{display:"block"}}>
+                <div className="add-mcq-question-box" onClick={duplicateMCQ}> 
+                    <button className="add-question-button"> + Add multiple choice question</button>
+                </div>
+                <div className="add-mcq-question-box">
+                    <button className="add-question-button" onClick={duplicateShortAnswer}> + Add short answer question</button>
+                </div>
+            </div>
         </div>
-        <div className="add-mcq-question-box">
-            <button className="add-question-button"> + Add short answer question</button>
         </div>
-        </div>
-        
-    </div> 
     );
 }
