00849e7564d704d2782b5ffcc4c4c6b7a97fd426
trishmanjunath <108712861+trishmanjunath@users.noreply.github.com>
2023-10-31 14:32:28 +1100

    fixed badge duplication, added ranking system

--- src/dashboard/components/teacher/TeacherCreateTest.js
+++ src/dashboard/components/teacher/TeacherCreateTest.js
@@ -1,69 +1,77 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
 import "../../style/dashboard.css";
 import "../../style/teachercreatetest.css";
 import "../../fonts/font.css";
 import BadgeIcon from "../../svg/badgeIcon";
 
 export const TeacherCreateTest = () => {
+  const [questions, setQuestions] = useState([
+    {
+      type: "mcq",
+      question: "",
+      options: ["", "", "", ""],
+      badge: null,
+    },
+    {
+      type: "shortAnswer",
+      question: "",
+      answer: "",
+      badge: null,
+    },
+  ]);
 
-  const [activeBadge, setActiveBadge] = useState(null);
-  const badges = ['#518CFE', '#6D5ED2', '#5ED278', '#F4694C', '#F4D94C'];
-  const inactiveBadge = '#ABABAB'
- 
-  const handleBadgeChange = (buttonId) => {
-    setActiveBadge(buttonId);
+  const badges = ["#518CFE", "#6D5ED2", "#5ED278", "#F4694C", "#F4D94C"];
+  const inactiveBadge = "#ABABAB";
+
+  const handleBadgeChange = (buttonId, index) => {
+    const updatedQuestions = [...questions];
+    updatedQuestions[index].badge = buttonId;
+    setQuestions(updatedQuestions);
   };
 
-  function resetMCQ(mcqElement) {
-    mcqElement.querySelector(".question-input-text").value = "";
-    mcqElement.querySelectorAll("input[type=radio]").forEach((radio) => {
-      radio.checked = false;
+  const resetMCQ = (index) => {
+    const updatedQuestions = [...questions];
+    updatedQuestions[index] = {
+      type: "mcq",
+      question: "",
+      options: ["", "", "", ""],
+      badge: null,
+    };
+    setQuestions(updatedQuestions);
+  };
+
+  const resetShortAnswer = (index) => {
+    const updatedQuestions = [...questions];
+    updatedQuestions[index] = {
+      type: "shortAnswer",
+      question: "",
+      answer: "",
+      badge: null,
+    };
+    setQuestions(updatedQuestions);
+  };
+
+  const duplicateMCQ = () => {
+    const updatedQuestions = [...questions];
+    updatedQuestions.push({
+      type: "mcq",
+      question: "",
+      options: ["", "", "", ""],
+      badge: null,
     });
-    mcqElement.querySelectorAll(".question-option-text").forEach((option) => {
-      option.value = "";
+    setQuestions(updatedQuestions);
+  };
+
+  const duplicateShortAnswer = () => {
+    const updatedQuestions = [...questions];
+    updatedQuestions.push({
+      type: "shortAnswer",
+      question: "",
+      answer: "",
+      badge: null,
     });
-    setActiveBadge(inactiveBadge); 
-  }
-
-  function resetShortAnswer(saElement) {
-    saElement.querySelector(".question-input-text").value = "";
-    saElement.querySelector(".sa-option-text").value = "";
-    
-    setActiveBadge(inactiveBadge); 
-  }
-
-  function duplicateMCQ() {
-   
-    // Find the element you want to duplicate by its ID
-    var originalMCQ = document.getElementById("mcq-form");
-
-    // Create a new element (a clone) based on the original component
-    var newMCQ = originalMCQ.cloneNode(true);
-
-    // Append the new component to the parent container
-    var container = document.getElementById("questions-container");
-    container.appendChild(newMCQ);
-
-    resetMCQ(newMCQ);
-
-  }
-
-  function duplicateShortAnswer() {
-
-      // Find the element you want to duplicate by its ID
-      var originalSA = document.getElementById("sa-form");
-
-      // Create a new element (a clone) based on the original component
-      var newSA = originalSA.cloneNode(true);
-  
-      // Append the new component to the parent container
-      var container = document.getElementById("questions-container");
-      container.appendChild(newSA);
-  
-      resetShortAnswer(newSA);
-      
-  }
+    setQuestions(updatedQuestions);
+  };
 
   return (
     <div>
@@ -73,155 +81,136 @@ export const TeacherCreateTest = () => {
             className="input-text"
             type="text"
             placeholder="Title of Quiz"
-          ></input>
+          />
         </div>
-        <div className="mcq-box" id="mcq-form">
-          <div style={{ display: "flex" }}>
-            <input
-              className="question-input-text"
-              type="text"
-              placeholder="Question..."
-            ></input>
-
-            <div>
-              {badges.map((badgeId) => (
-                <button
-                  className="badge-button"
-                  key={badgeId}
-                  // className={activeBadge === badgeId ? 'active' : ''}
-                  onClick={() => handleBadgeChange(badgeId)}
-                >
-                  <BadgeIcon fill={activeBadge === badgeId ? badgeId : inactiveBadge} />
-                </button>
-              ))}
-            </div>
+        {questions.map((question, index) => (
+          <div key={index}>
+            {question.type === "mcq" ? (
+              <div className="mcq-box">
+                <div style={{ display: "flex" }}>
+                  <input
+                    className="question-input-text"
+                    type="text"
+                    placeholder="Question..."
+                    value={question.question}
+                    onChange={(e) => {
+                      const updatedQuestions = [...questions];
+                      updatedQuestions[index].question = e.target.value;
+                      setQuestions(updatedQuestions);
+                    }}
+                  />
+                  <div>
+                    {badges.map((badgeId) => (
+                      <button
+                        className="badge-button"
+                        key={badgeId}
+                        onClick={() => handleBadgeChange(badgeId, index)}
+                      >
+                        <BadgeIcon
+                          fill={
+                            question.badge === badgeId
+                              ? badgeId
+                              : inactiveBadge
+                          }
+                        />
+                      </button>
+                    ))}
+                  </div>
+                </div>
+                <hr className="line" />
+                <div>
+                  <form>
+                    {question.options.map((option, optionIndex) => (
+                      <div key={optionIndex}>
+                        <input
+                          type="radio"
+                          id={`option${optionIndex}`}
+                          name={`radio${index}`}
+                          style={{
+                            width: "18px",
+                            height: "18px",
+                            outline: "none",
+                            border: "0.5px solid #6d6b6b",
+                            display: "block",
+                            alignItems: "center",
+                            marginTop: "20px",
+                          }}
+                        />
+                        <input
+                          className="question-option-text"
+                          style={{ marginTop: "-22px" }}
+                          type="text"
+                          placeholder={`Option ${optionIndex + 1}`}
+                          value={option}
+                          onChange={(e) => {
+                            const updatedQuestions = [...questions];
+                            updatedQuestions[index].options[
+                              optionIndex
+                            ] = e.target.value;
+                            setQuestions(updatedQuestions);
+                          }}
+                        />
+                      </div>
+                    ))}
+                  </form>
+                </div>
+              </div>
+            ) : (
+              <div className="short-answer-box">
+                <div style={{ display: "flex" }}>
+                  <input
+                    className="question-input-text"
+                    type="text"
+                    placeholder="Question..."
+                    value={question.question}
+                    onChange={(e) => {
+                      const updatedQuestions = [...questions];
+                      updatedQuestions[index].question = e.target.value;
+                      setQuestions(updatedQuestions);
+                    }}
+                  />
+                  <div>
+                    {badges.map((badgeId) => (
+                      <button
+                        className="badge-button"
+                        key={badgeId}
+                        onClick={() => handleBadgeChange(badgeId, index)}
+                      >
+                        <BadgeIcon
+                          fill={
+                            question.badge === badgeId
+                              ? badgeId
+                              : inactiveBadge
+                          }
+                        />
+                      </button>
+                    ))}
+                  </div>
+                </div>
+                <hr className="line" />
+                <div>
+                  <input
+                    className="sa-option-text"
+                    type="text"
+                    placeholder="Type your answer..."
+                    value={question.answer}
+                    onChange={(e) => {
+                      const updatedQuestions = [...questions];
+                      updatedQuestions[index].answer = e.target.value;
+                      setQuestions(updatedQuestions);
+                    }}
+                  />
+                </div>
+              </div>
+            )}
           </div>
-          <hr className="line"></hr>
-          <div>
-            <form>
-              <input
-                type="radio"
-                id="option1"
-                name="newradio"
-                style={{
-                  width: "18px",
-                  height: "18px",
-                  outline: "none",
-                  border: "0.5px solid #6d6b6b;",
-                  display: "block",
-                  alignItems: "center",
-                  marginTop: "20px",
-                }}
-              ></input>
-              <input
-                className="question-option-text"
-                style={{ marginTop: "-22px" }}
-                type="text"
-                placeholder="Option 1"
-              ></input>
-
-              <input
-                type="radio"
-                id="option2"
-                name="newradio"
-                style={{
-                  width: "18px",
-                  height: "18px",
-                  outline: "none",
-                  border: "0.5px solid #6d6b6b;",
-                  display: "block",
-                  alignItems: "center",
-                  marginTop: "20px",
-                }}
-              ></input>
-              <input
-                className="question-option-text"
-                style={{ marginTop: "-22px" }}
-                type="text"
-                placeholder="Option 2"
-              ></input>
-
-              <input
-                type="radio"
-                id="option3"
-                name="newradio"
-                style={{
-                  width: "18px",
-                  height: "18px",
-                  outline: "none",
-                  border: "0.5px solid #6d6b6b;",
-                  display: "block",
-                  alignItems: "center",
-                  marginTop: "20px",
-                }}
-              ></input>
-              <input
-                className="question-option-text"
-                style={{ marginTop: "-22px" }}
-                type="text"
-                placeholder="Option 3"
-              ></input>
-
-              <input
-                type="radio"
-                id="option4"
-                name="newradio"
-                style={{
-                  width: "18px",
-                  height: "18px",
-                  outline: "none",
-                  border: "0.5px solid #6d6b6b;",
-                  display: "block",
-                  alignItems: "center",
-                  marginTop: "20px",
-                }}
-              ></input>
-              <input
-                className="question-option-text"
-                style={{ marginTop: "-22px" }}
-                type="text"
-                placeholder="Option 4"
-              ></input>
-            </form>
-          </div>
-        </div>
-
-        <div className="short-answer-box" id="sa-form" >
-        <div style={{display:"flex"}}>
-          <input 
-            className="question-input-text"
-            type="text"
-            placeholder="Question..."
-            id="sa-question"
-          ></input>
-         <div>
-              {badges.map((badgeId) => (
-                <button
-                  className="badge-button"
-                  key={badgeId}
-                  // className={activeBadge === badgeId ? 'active' : ''}
-                  onClick={() => handleBadgeChange(badgeId)}
-                >
-                  <BadgeIcon fill={activeBadge === badgeId ? badgeId : inactiveBadge} />
-                </button>
-              ))}
-            </div>
-            </div> 
-          <hr className="line"></hr>
-          <div>
-            <input
-              className="sa-option-text"
-              type="text"
-              placeholder="Type your answer..."
-              id="sa-answer"
-            ></input>
-          </div>
-
-        </div>
+        ))}
       </div>
-
       <div id="buttons">
-        <div id="form-clones" style={{ display: "block", paddingBottom: "20px" }}>
+        <div
+          id="form-clones"
+          style={{ display: "block", paddingBottom: "20px" }}
+        >
           <div className="add-mcq-question-box" onClick={duplicateMCQ}>
             <text className="add-question-button">
               + Add multiple choice question
@@ -234,10 +223,10 @@ export const TeacherCreateTest = () => {
           </div>
         </div>
       </div>
-
       <div>
         <button className="test-next-button">Next</button>
       </div>
     </div>
   );
 };
+
--- src/dashboard/components/teacher/rankingSystem.js
+++ src/dashboard/components/teacher/rankingSystem.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "../../style/rankingsystem.css";
+import RankingSystemBox from "../../svg/rankingSystemBox";
 
 export const RankingSystem = () =>{
 
 return(
     <div className="ranking-box">
-      
+       <RankingSystemBox/>
     </div>
 );
 }
